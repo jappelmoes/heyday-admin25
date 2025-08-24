0252c32ef1fdf96e40dd4e12c77138d315beac8f
codeinkbe <145548075+codeinkbe@users.noreply.github.com>
2025-08-24 22:00:37 +0200

    Importing GitHub Repository

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,38 +3,39 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import {
   ClerkProvider,
+  SignedIn,
+  SignedOut,
   SignInButton,
   UserButton,
+  useUser,
 } from "@clerk/nextjs";
-import { auth } from "@clerk/nextjs/server";
 import Link from "next/link";
 const inter = Inter({ subsets: ["latin"] });
 
-export default async function RootLayout({
+export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const { userId } = await auth();
-
   return (
     <ClerkProvider>
       <html lang="en" className={inter.className}>
         <body className="container mx-auto">
-          {userId ? (
+          <SignedIn>
             <nav className="navbar bg-base-200 rounded-2xl px-6 justify-between">
               <p>Welcome back</p>
               <Link href="/">Home</Link>
               <UserButton />
             </nav>
             <div className="flex justify-center">{children}</div>
-          ) : (
+          </SignedIn>
+          <SignedOut>
             <div className="flex flex-col items-center justify-center h-screen gap-4">
               <div className="text-2xl font-bold">Heyday Admin</div>
               <div>Please sign in</div>
                 <SignInButton />
             </div>
-          )}
+          </SignedOut>
         </body>
       </html>
     </ClerkProvider>
