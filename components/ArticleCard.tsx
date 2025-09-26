4f912f8989579df2dd53bc7336ccb50c8950c728
codeinkbe <145548075+codeinkbe@users.noreply.github.com>
2025-09-26 12:49:19 +0200

    Updated ArticleCard.tsx

--- components/ArticleCard.tsx
+++ components/ArticleCard.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Link from "next/link";
-import { Calendar, Edit, Eye } from "lucide-react";
+import { Calendar, CreditCard as Edit, Eye } from "lucide-react";
 
 interface ArticleCardProps {
   id: string;
